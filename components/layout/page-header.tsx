2cd2708cc19b368a631b26519d214a5eea590ce9
jacko06v <jacomosconi@gmail.com>
2024-09-13 09:37:44 +0200

    fix: fix

--- components/layout/page-header.tsx
+++ components/layout/page-header.tsx
@@ -38,7 +38,7 @@ function PageHeaderHeading({
     <motion.h1
       variants={fadeDownVariant()}
       className={cn(
-        "bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-4xl font-bold leading-tight tracking-tight text-transparent drop-shadow-sm dark:from-white dark:to-white md:text-8xl md:leading-[6rem] lg:leading-[1.1]",
+        "bg-gradient-to-br from-white to-white bg-clip-text text-center text-4xl font-bold leading-tight tracking-tight text-transparent drop-shadow-sm dark:from-white dark:to-white md:text-8xl md:leading-[6rem] lg:leading-[1.1]",
         className
       )}
       {...props}
