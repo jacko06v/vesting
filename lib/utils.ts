31854cca7a6a9561a839087f121753712f42ebd1
jacko06v <jacomosconi@gmail.com>
2024-12-24 14:41:00 +0100

    fix(chore): fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function trimFormattedBalance(balance: string | undefined, decimals = 4) {
+  if (!balance) {
+    return "0"
+  }
+  const [integer, decimal] = balance.split(".")
+  if (!decimal) return integer
+
+  const trimmedDecimal = decimal.slice(0, decimals)
+  return `${integer}.${trimmedDecimal}`
+}
